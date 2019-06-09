28b6ad6947b1db708f7850fb6ee3db06d633757d
jiangyitao <451988022@163.com>
2019-06-09 18:39:16 +0800

    update

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Vue Element Admin',
+  title: 'fgnb',
 
   /**
    * @type {boolean} true | false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">fgnb</h3>
       </div>
 
       <el-form-item prop="username">
