42ead3a547de9950b4c43559376c7bec241e43f4
jiangyitao <451988022@163.com>
2019-08-09 17:15:16 +0800

    update

--- src/pages/mobile/components/MobileCapture.vue
+++ src/pages/mobile/components/MobileCapture.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="width: 1400px;height: 850px;margin-bottom: 10px">
-    <i class="el-icon-refresh" style="font-size: 20px;color: green" title="重新获取" @click="refresh" />
-    <i class="el-icon-circle-plus" style="font-size: 20px;color: black" title="添加page" @click="addPage" />
+    <i class="el-icon-refresh" style="font-size: 20px;color: green;cursor: pointer" title="重新获取" @click="refresh" />
+    <i class="el-icon-circle-plus" style="font-size: 20px;color: black;cursor: pointer" title="添加page" @click="addPage" />
     <!-- inspector -->
     <div>
       <mobile-inspector :canvas-id="canvasId" :img-info="imgInfo" :window-hierarchy-json="windowHierarchyJson" :tree-loading="treeLoading" />
