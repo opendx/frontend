93edebc7b86e6176d540f4d2897015b08fac5f2d
jiangyitao <451988022@163.com>
2019-10-27 22:52:57 +0800

    update

--- src/pages/testtask/report/components/TestCase.vue
+++ src/pages/testtask/report/components/TestCase.vue
@@ -11,45 +11,41 @@
           </el-tag>
           <el-tag @click="$router.push('/action/testcase/update/'+testcase.id)" size="small" type="info">查看用例</el-tag>
         </template>
-        <div style="float: left">
-          <video :src="testcase.videoUrl" width="300px" controls="controls">浏览器不支持video标签</video>
-        </div>
-        <div style="float: left;margin-left: 5px;max-height: 600px;overflow: auto">
-          <el-table :data="testcase.steps" border>
-            <el-table-column prop="number" label="步骤" align="center" width="50px" />
-            <el-table-column prop="name" label="步骤名" align="center" width="200px" />
-            <el-table-column prop="startTime" label="开始时间" align="center" width="160px" />
-            <el-table-column prop="endTime" label="结束时间" align="center" width="160px" />
-            <el-table-column label="耗时" align="center" width="100px">
-              <template scope="{ row }">
-                {{ row.endTime ? parseInt(new Date(row.endTime) - new Date(row.startTime)) / 1000 + 's' : '-' }}
-              </template>
-            </el-table-column>
-            <el-table-column label="状态" align="center" width="90px">
-              <template scope="{ row }">
-                <el-tag :type="row.endTime ? 'success' : row.startTime ? 'danger' : 'info'" style="width: 65px;">
-                  {{ row.endTime ? '完成' : row.startTime ? '失败' : '未执行' }}
-                </el-tag>
-              </template>
-            </el-table-column>
-          </el-table>
-        </div>
-        <div style="float: left;margin-left: 5px" v-if="testcase.failInfo || testcase.failImgUrl">
-          <el-table :data="[testcase]" border>
-            <el-table-column label="失败信息" align="center" width="280px">
-              <template scope="{ row }">
-                <span style="color:red" v-if="row.failInfo">{{ row.failInfo }}</span>
-                <span v-else>-</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="失败时截图" align="center" width="320px">
-              <template scope="{ row }">
-                <img v-if="row.failImgUrl" :src="row.failImgUrl" style="width: 300px" />
-                <span v-else>-</span>
-              </template>
-            </el-table-column>
-          </el-table>
-        </div>
+        <el-row :gutter="2">
+          <el-col :span="4">
+            <video :src="testcase.videoUrl" width="100%" controls="controls">浏览器不支持video标签</video>
+          </el-col>
+          <el-col :span="12">
+            <el-table :data="testcase.steps" border max-height="700">
+              <el-table-column prop="number" label="步骤" align="center" width="70px" />
+              <el-table-column prop="name" label="步骤名" align="center" />
+              <el-table-column prop="startTime" label="开始时间" align="center" width="200px" />
+              <el-table-column prop="endTime" label="结束时间" align="center" width="200px" />
+              <el-table-column label="耗时" align="center">
+                <template scope="{ row }">
+                  {{ row.endTime ? parseInt(new Date(row.endTime) - new Date(row.startTime)) / 1000 + '秒' : '-' }}
+                </template>
+              </el-table-column>
+              <el-table-column label="状态" align="center" width="90px">
+                <template scope="{ row }">
+                  <el-tag :type="row.endTime ? 'success' : row.startTime ? 'danger' : 'info'" style="width: 65px;">
+                    {{ row.endTime ? '完成' : row.startTime ? '失败' : '未执行' }}
+                  </el-tag>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-col>
+          <el-col :span="8">
+            <el-col>
+              <el-col :span="8">
+                <img v-if="testcase.failImgUrl" :src="testcase.failImgUrl" width="100%"/>
+              </el-col>
+              <el-col :span="16">
+                <div style="color:red;max-height: 700px;overflow: auto" v-if="testcase.failInfo">{{ testcase.failInfo }}</div>
+              </el-col>
+            </el-col>
+          </el-col>
+        </el-row>
       </el-collapse-item>
     </el-collapse>
   </div>
