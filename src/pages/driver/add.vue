44799d18caf2ea5333c0ea2cfdb98abb84253fd6
jiangyitao <451988022@163.com>
2019-11-07 00:43:50 +0800

    update

---
+++ src/pages/driver/add.vue
@@ -0,0 +1,16 @@
+<template>
+  <save-driver-dialog :is-add="true" />
+</template>
+
+<script>
+import SaveDriverDialog from './components/SaveDriverDialog'
+export default {
+  components: {
+    SaveDriverDialog
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/driver/update.vue
@@ -0,0 +1,16 @@
+<template>
+  <save-driver-dialog :is-add="false" />
+</template>
+
+<script>
+import SaveDriverDialog from './components/SaveDriverDialog'
+export default {
+  components: {
+    SaveDriverDialog
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
